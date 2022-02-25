9228385ec404492312285a3d89b961628e5b570f
Bryan31542 <00063820@uca.edu.sv>
2022-02-25 15:15:53 -0600

    Looping the menu with promises :sparkles:

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ console.clear();
 
 const main = async () => {
   console.log("Hola mundo".green);
-  showMenu();
-  // pause();
+
+  let opt = "";
+
+  do {
+    opt = await showMenu();
+    console.log(`\nHas seleccionado la opción: ${opt.green}\n`);
+
+    if (opt !== "0") await pause();
+  } while (opt !== "0");
 };
 
 main();
--- helpers/messages.js
+++ helpers/messages.js
@@ -3,37 +3,43 @@ const { read } = require("fs");
 require("colors");
 
 const showMenu = () => {
-  console.clear();
-  console.log("====================================".green);
-  console.log("\tSeleccion una opción");
-  console.log("====================================\n".green);
+  return new Promise((resolve) => {
+    console.clear();
+    console.log("====================================".green);
+    console.log("\tSeleccion una opción");
+    console.log("====================================\n".green);
 
-  console.log(`${"1.".green} Crear una nueva tarea`);
-  console.log(`${"2.".green} Mostrar tareas`);
-  console.log(`${"3.".green} Mostrar tareas completadas`);
-  console.log(`${"4.".green} Mostrar tareas pendientes`);
-  console.log(`${"5.".green} Completar tareas`);
-  console.log(`${"6.".green} Borrar tareas`);
-  console.log(`${"0.".green} Salir\n`);
+    console.log(`${"1.".green} Crear una nueva tarea`);
+    console.log(`${"2.".green} Mostrar tareas`);
+    console.log(`${"3.".green} Mostrar tareas completadas`);
+    console.log(`${"4.".green} Mostrar tareas pendientes`);
+    console.log(`${"5.".green} Completar tareas`);
+    console.log(`${"6.".green} Borrar tareas`);
+    console.log(`${"0.".green} Salir\n`);
 
-  const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  readline.question("Seleccione una opción: ", (opt) => {
-    readline.close();
+    readline.question("Seleccione una opción: ", (opt) => {
+      readline.close();
+      resolve(opt);
+    });
   });
 };
 
 const pause = () => {
-  const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  return new Promise((resolve) => {
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  readline.question(`Presione ${"ENTER".green} para continuar`, (opt) => {
-    readline.close();
+    readline.question(`Presione ${"ENTER".green} para continuar`, (opt) => {
+      readline.close();
+      resolve();
+    });
   });
 };
 
